e1d839556e7600d4a2421ebb974d6dca3c6382a6
tahos81 <tahirozpala@gmail.com>
2022-06-11 12:07:22 +0200

    folder tree

--- contracts/DonateOnDEX.sol
+++ contracts/DonateOnDEX.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import "./IUniswapV2Router01.sol";
+import "./interfaces/IUniswapV2Router01.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract DonateOnDEX {
--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
-import "./IStarknetCore.sol";
+import "./interfaces/IStarknetCore.sol";
 
 contract Marketplace is ERC721Holder {
 
