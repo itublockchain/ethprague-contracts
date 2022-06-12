033cc328f3cda22b832d5152b8f1915bc6c71c62
tahos81 <tahirozpala@gmail.com>
2022-06-12 09:50:43 +0200

    commented Lender

--- contracts/Lender.sol
+++ contracts/Lender.sol
@@ -3,6 +3,12 @@ pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/*
+    Non-profit lending protocol using bct Carbon token as collateral, lending fee is taken as a percentage of the collateralized carbon tokens.
+    which gets locked in this contract(burned). Burned carbon tokens are carbon emissions offseted in real life.
+    this protocol uses its other products as a source of income to fund its matic pool.
+*/
+
 contract Lender {
 
     struct Loan {
@@ -11,9 +17,9 @@ contract Lender {
         uint256 endTime;
     }
 
-    IERC20 immutable carbonToken;
+    IERC20 immutable carbonToken; //base carbon tonne
 
-    mapping(address => Loan) loans;
+    mapping(address => Loan) loans; //users can't take new loans before repaying
 
     uint256 constant oneWeekInterest = 5; //percent
     uint256 constant twoWeekInterest = 10; //percent
@@ -27,11 +33,12 @@ contract Lender {
     }
 
     function takeLoan(uint256 collateralAmount, uint256 duration) external {
+        require(loans[msg.sender].endTime == 0);
+        //User must approve this contract before taking a loan
         require(carbonToken.transferFrom(msg.sender, address(this), collateralAmount));
 
+        //Calculate amount of matic to lend
         uint maticLendAmount;
-
-        address payable borrower = payable(msg.sender);
 
         if (duration == 7 days) {
             maticLendAmount = collateralAmount * maticPriceOfCarbon * (100 - oneWeekInterest) / 100;
@@ -42,6 +49,8 @@ contract Lender {
         } else if (duration == 28 days) {
             maticLendAmount = collateralAmount * maticPriceOfCarbon * (100 - fourWeekInterest) / 100;
         }
+
+        address payable borrower = payable(msg.sender);
 
         borrower.transfer(maticLendAmount);
 
@@ -54,10 +63,11 @@ contract Lender {
         loans[msg.sender] = loan;
     }
 
-    function repayLoan() external payable{
-        require(msg.value == loans[msg.sender].lendedMatic);
-        require(block.timestamp <= loans[msg.sender].endTime);
-        
+    function repayLoan() external payable { 
+        require(msg.value == loans[msg.sender].lendedMatic); //User sends back the lended matic
+        require(block.timestamp <= loans[msg.sender].endTime);  //If user does not pay back in time he/she loses money and he/she can't take a loan ever again.
+
+        //we give back half the interest we took to incentivize repayment of loans, other half is carbon tokens locked in this contract which is equivalent to burning.
         uint256 carbonRepayAmount = (loans[msg.sender].borrowedCarbon + (loans[msg.sender].lendedMatic / maticPriceOfCarbon)) / 2;
         require(carbonToken.transferFrom(address(this), msg.sender, carbonRepayAmount));
 
